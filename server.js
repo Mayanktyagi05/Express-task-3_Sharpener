2b335efbc6a8a183c148a1699f51a3094cf9d901
Mayanktyagi05 <mayanktyagee@gmail.com>
2023-12-17 11:16:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1> Page Not Found</h1>');
+});
+app.listen(4000);
